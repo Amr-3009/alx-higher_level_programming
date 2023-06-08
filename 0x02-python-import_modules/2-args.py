91b10a16b594f1860bee5777aa7a2445f14ebf20
Amr-3009 <amrosama1k@gmail.com>
2023-06-08 08:18:37 -0700

    gg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
     length = len(argv) - 1
     if length == 0:
-        print("{:d} argumets.".format(length))
+        print("{}".format("0 arguments."))
     elif length == 1:
-        print("{:d} argument:".format(length))
-        print("{:d}: {}".format(length, argv[1]))
+        print("{}".format("1 argument:"))
+        print("1: {}".format(argv[1]))
     else:
-        print("{:d} argumets:".format(length))
-        for n in range(1, length + 1):
-            print("{:d}: {}".format(n, argv[n]))
+        print("{:d} {}".format(length, "arguments:"))
+        for i in range(1, length + 1):
+            print("{:d}: {}".format(i, argv[i]))
