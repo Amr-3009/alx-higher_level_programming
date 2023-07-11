4c3a79b4eb9c35ea736fb8efdccdc7f49c82d3a1
Amr-3009 <amrosama1k@gmail.com>
2023-07-11 05:34:44 -0700

    ff

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function"""
 
 
 def read_file(filename=""):
+    """Prints contents of texxt file to stdout"""
+
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
