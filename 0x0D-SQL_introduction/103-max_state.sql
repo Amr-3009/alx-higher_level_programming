4871772c41ba546b5f98cf58351ac2366799ffa6
Amr-3009 <amrosama1k@gmail.com>
2023-08-15 14:17:29 -0700

    hh

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- comment
-SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperature` GROUP BY `state` ORDER BY `state`;
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
