98814f036d818ef5e19edb733ccea1f56924c1fb
Amr-3009 <125468028+Amr-3009@users.noreply.github.com>
2023-12-02 13:44:58 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// Prints a string
-
-const myVar = 'Javascript is amazing';
-console.log(myVar);
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-// concats
-
-console.log(process.argv[2] + ' is ' + process.argv[3]);
