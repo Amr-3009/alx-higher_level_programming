662b069a12875788fc63bdccc6683bce12fd6381
Amr-3009 <amrosama1k@gmail.com>
2023-12-11 20:17:41 +0200

    gg

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+#!/user/bin/node
+// random comment
+
+if (process.argv.length === 2) {
+	  console.log("No argument");
+} else if (process.argv.length === 3 or process.argv) {
+	  console.log("Argument found");
+} else {
+	  console.log("Arguments found");
+}
