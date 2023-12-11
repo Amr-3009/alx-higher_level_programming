4e8f9ce5fe4c870a9574fec93cab43944c6528aa
Amr-3009 <amrosama1k@gmail.com>
2023-12-11 22:18:02 +0200

    gg

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const list = process.argv.sort().map(Number).slice(2);
+  const list = process.argv.slice(2).map(Number).sort();
   console.log(list.reverse()[1]);
 }
