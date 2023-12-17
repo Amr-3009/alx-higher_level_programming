fb472e927953f6148c8785226653157a8ce6e04a
Amr-3009 <amrosama1k@gmail.com>
2023-12-17 08:53:43 -0800

    ff

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,8 @@
 """
 MySQL Database Query Script
 
-This script connects to a MySQL database and retrieves all rows from the "states" table.
+This script connects to a MySQL database and retrieves all rows from the
+"states" table.
 
 Usage:
     python script.py <username> <password> <database_name>
@@ -13,17 +14,20 @@ Parameters:
     <database_name>: Name of the MySQL database.
 
 Dependencies:
-    - MySQLdb module (MySQL-python): This script requires the MySQLdb module to be installed. You can install it using:
+    - MySQLdb module (MySQL-python): This script requires the MySQLdb module
+    to be installed. You can install it using:
         pip install MySQL-python
 
 Example:
     python script.py myuser mypassword mydatabase
 
 Note:
-    Before running the script, make sure to replace "myuser," "mypassword," and "mydatabase" with your MySQL username, password, and database name.
+    Before running the script, make sure to replace "myuser," "mypassword,"
+    and "mydatabase" with your MySQL username, password, and database name.
 
 Script Workflow:
-1. Establish a connection to the specified MySQL database using the provided username, password, and database name.
+1. Establish a connection to the specified MySQL database using the provided
+    username, password, and database name.
 2. Create a cursor object to interact with the database.
 3. Execute an SQL query to select all rows from the "states" table.
 4. Fetch all rows returned by the query.
@@ -37,7 +41,8 @@ from sys import argv
 
 if __name__ == '__main__':
     # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
     # Create a cursor object to interact with the database
     cur = db.cursor()
@@ -57,4 +62,3 @@ if __name__ == '__main__':
 
     # Close the database connection
     db.close()
-
