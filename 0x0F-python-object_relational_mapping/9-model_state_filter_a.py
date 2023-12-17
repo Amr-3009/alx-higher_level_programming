793bff49bfcd8025d46a342d1e83a87e22ddbd89
Amr-3009 <amrosama1k@gmail.com>
2023-12-17 10:05:01 -0800

    gg

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,53 +1,26 @@
 #!/usr/bin/python3
 """
-Query States Ending in 'a' from the Database
-
-This script connects to a MySQL database using SQLAlchemy and queries and print
-information about states from the "states" table whose names end with 'a'.
-
-Usage:
-    python script_query_states_with_a.py <username> <password> <database_name>
-
-Parameters:
-    <username>: MySQL database username.
-    <password>: Password for the specified user.
-    <database_name>: Name of the MySQL database.
-
-Dependencies:
-    - SQLAlchemy: This script requires the SQLAlchemy library. You can install
-      it using:
-          pip install sqlalchemy
-    - model_state.py: Contains the State model definition.
-
-Example:
-    python script_query_states_with_a.py myuser mypassword mydatabase
+Script that lists all State objects that contain the letter a from the database
+Using module SQLAlchemy
 """
 
 from model_state import Base, State
-from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 
 if __name__ == "__main__":
-    # Create a SQLAlchemy engine to connect to the MySQL database
+    # create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
-    # Create a session to interact with the database
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
-
-    # Ensure the "states" table is created in the database
     Base.metadata.create_all(engine)
 
-    # Query states from the "states" table whose names end with 'a'
-    # and print information
-    states = session.query(State).filter(State.name.like('%a'))\
-        .order_by(State.id).all()
-
-    # Print the information for each state
-    for state in states:
+    s_tate = session.query(State).filter(State.name.like('%a%'))\
+                                 .order_by(State.id).all()
+    for state in s_tate:
         print("{}: {}".format(state.id, state.name))
-
-    # Close the database session
     session.close()
